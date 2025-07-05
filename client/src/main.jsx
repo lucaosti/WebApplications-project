c95ca6fed93192821a44afc2ff8f4eaaf2cd2906
Luca Ostinelli <s347489@studenti.polito.it>
2025-07-05 08:47:42 +0200

    Update CSS

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './auth/AuthContext.jsx';
-import './index.css';
 import App from './App.jsx';
 
 /**
